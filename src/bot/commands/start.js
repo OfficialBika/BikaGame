548467269befaab94028ff0f3ea7eaf0e6bb8689
Official Bika <phoetaay@gmail.com>
2026-04-12 15:22:47 +0700

    Add files via upload

--- src/bot/commands/start.js
+++ src/bot/commands/start.js
@@ -1,1 +1,10 @@
-
+module.exports = function registerStart(bot, { userService }, config) {
+  bot.start(async (ctx) => {
+    const tgUser = ctx.from;
+    if (!tgUser) return;
+    // Ensure user and award start bonus if they are new
+    const user = await userService.ensureUser(tgUser, config.startBonus);
+    const welcome = `Welcome, ${tgUser.first_name || 'Player'}! You now have ${config.coin} ${user.balance}. Enjoy the game!`;
+    await ctx.reply(welcome);
+  });
+};
