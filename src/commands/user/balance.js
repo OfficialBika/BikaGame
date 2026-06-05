20841e310a606606ec11ae325d47b4fa42a5d0c9
Official Bika <phoetaay@gmail.com>
2026-06-05 22:11:49 +0700

    Add files via upload

--- src/commands/user/balance.js
+++ src/commands/user/balance.js
@@ -1,2 +1,41 @@
-const { COIN } = require('../../config/constants'); const { getUser } = require('../../services/economyService'); const { replyHTML } = require('../../utils/telegram'); const { fmt } = require('../../utils/format');
-module.exports = (bot)=>{ async function send(ctx){ const u=await getUser(ctx.from.id); return replyHTML(ctx,`💼 Balance: <b>${fmt(u?.balance)}</b> ${COIN}`); } bot.command('balance',send); bot.command('bal',send); };
+'use strict';
+
+const { COIN } = require('../../config/constants');
+const { getUser } = require('../../services/economyService');
+const { replyHTML } = require('../../utils/telegram');
+const { fmt } = require('../../utils/format');
+const { mentionHtml } = require('../../utils/helpers');
+
+function balanceText(ctx, user) {
+  const balance = Number(user?.balance || 0);
+  const totalWon = Number(user?.totalWon || 0);
+  const totalLost = Number(user?.totalLost || 0);
+  const isVip = Boolean(user?.isVip);
+
+  return (
+    `💼 <b>BIKA Wallet</b>\n` +
+    `━━━━━━━━━━━━━━\n` +
+    `👤 ${mentionHtml(ctx.from)}\n` +
+    `💰 Balance: <b>${fmt(balance)}</b> ${COIN}\n` +
+    `🏆 Total Won: <b>${fmt(totalWon)}</b> ${COIN}\n` +
+    `💸 Total Lost: <b>${fmt(totalLost)}</b> ${COIN}\n` +
+    `⭐ VIP: <b>${isVip ? 'YES' : 'NO'}</b>\n` +
+    `━━━━━━━━━━━━━━`
+  );
+}
+
+module.exports = (bot) => {
+  async function sendBalance(ctx) {
+    const user = await getUser(ctx.from.id);
+
+    return replyHTML(ctx, balanceText(ctx, user));
+  }
+
+  // Slash commands
+  bot.command('bal', sendBalance);
+  bot.command('balance', sendBalance);
+  bot.command('mybalance', sendBalance);
+
+  // Dot commands
+  bot.hears(/^\.(bal|balance|mybalance)\s*$/i, sendBalance);
+};
