846ccdbbc0779db4449539c15738954432e61041
Official Bika <phoetaay@gmail.com>
2026-09-24 18:17:17 +0700

    feat(2d): moderate betting event comments

--- src/handlers/messageHandler.js
+++ src/handlers/messageHandler.js
@@ -1,1 +1,16 @@
-module.exports = (bot)=>{};
+'use strict';
+
+const twoD = require('../services/twoDService');
+const logger = require('../utils/logger');
+
+module.exports = function (bot) {
+  bot.on('message', async function (ctx, next) {
+    try {
+      const handled = await twoD.handleComment(ctx, bot);
+      if (handled) return;
+    } catch (err) {
+      logger.error('2D message moderation error', err);
+    }
+    return next();
+  });
+};
